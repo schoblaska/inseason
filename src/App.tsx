226405a59d29a26d43f09f3b6c2dcfe82d87ed66
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-08-25 13:38:12 -0500

    refactor App.js into typed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, Dispatch, SetStateAction } from "react";
 import "./App.css";
+
+const GithubBanner = () => (
+  <a href="https://github.com/joeyschoblaska/inseason">
+    <img
+      style={{ position: "absolute", top: 0, right: 0, border: 0 }}
+      src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+      alt="Fork me on GitHub"
+    />
+  </a>
+);
+
+const Header = ({ stateKey }: { stateKey: string }) => (
+  <div className="header-image">
+    <img src={`/images/headers/${stateKey.toLowerCase()}.png`} alt="banner" />
+  </div>
+);
+
+const Controls = ({
+  stateKeys,
+  stateKey,
+  setStateKey,
+}: {
+  stateKeys: string[];
+  stateKey: string;
+  setStateKey: Dispatch<SetStateAction<string>>;
+}) => (
+  <div className="controls">
+    <form id="controls">
+      <input className="search" type="text" placeholder="search" />
+      <select
+        className="state-selector"
+        value={stateKey}
+        onChange={(e) => setStateKey(e.target.value)}
+      >
+        {stateKeys.map((key) => (
+          <option key={key} value={key}>
+            {key}
+          </option>
+        ))}
+      </select>
+    </form>
+  </div>
+);
 
 function App() {
   const [stateKey, setStateKey] = useState("IL");
@@ -8,33 +51,14 @@ function App() {
 
   return (
     <div>
-      <a href="https://github.com/joeyschoblaska/inseason">
-        <img
-          style={{ position: "absolute", top: 0, right: 0, border: 0 }}
-          src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
-          alt="Fork me on GitHub"
-        />
-      </a>
+      <GithubBanner />
       <div className="container">
-        <div className="header-image">
-          <img src={`/images/headers/${stateKey.toLowerCase()}.png`} />
-        </div>
-        <div className="controls">
-          <form id="controls">
-            <input className="search" type="text" placeholder="search" />
-            <select
-              className="state-selector"
-              value={stateKey}
-              onChange={(e) => setStateKey(e.target.value)}
-            >
-              {stateKeys.map((key) => (
-                <option key={key} value={key}>
-                  {key}
-                </option>
-              ))}
-            </select>
-          </form>
-        </div>
+        <Header stateKey={stateKey} />
+        <Controls
+          stateKey={stateKey}
+          setStateKey={setStateKey}
+          stateKeys={stateKeys}
+        />
       </div>
     </div>
   );
