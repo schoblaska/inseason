e7d4421156943513601815bcd3e1c43027481cdc
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-08-26 08:48:38 -0500

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => (
       <div id="headshot"></div>
       <p>
         This app was written by me, Joey Schoblaska. You can check out some of
-        the other things I'm working on by visiting my
-        <a href="http://joeyschoblaska.com">website</a> or my
+        the other things I'm working on by visiting my{" "}
+        <a href="http://schoblaska.org">website</a> or my{" "}
         <a href="http://github.com/joeyschoblaska">github profile</a>. You can
         also follow me on twitter at{" "}
         <a href="http://twitter.com/joeyschoblaska">@joeyschoblaska</a>.
