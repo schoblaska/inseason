bd9599534f9e5e42a5e398bf369695813d04a7de
Joey Schoblaska <joey.schoblaska@gmail.com>
2022-08-26 08:39:17 -0500

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+const Footer = () => (
+  <div className="footer-container">
+    <div className="footer">
+      <div id="headshot"></div>
+      <p>
+        This app was written by me, Joey Schoblaska. You can check out some of
+        the other things I'm working on by visiting my
+        <a href="http://joeyschoblaska.com">website</a> or my
+        <a href="http://github.com/joeyschoblaska">github profile</a>. You can
+        also follow me on twitter at{" "}
+        <a href="http://twitter.com/joeyschoblaska">@joeyschoblaska</a>.
+      </p>
+      <p>
+        Thanks to{" "}
+        <a href="http://localfoods.about.com/od/searchbystate/State_Seasonal_Produce_Guides.htm">
+          about.com
+        </a>{" "}
+        for the original dataset.
+      </p>
+      <p className="clear"></p>
+    </div>
+  </div>
+);
+
+export default Footer;
